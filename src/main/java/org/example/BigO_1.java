e6c13682725c2781a199c0c841c4c062a438c909
shakibaenur <shakibaenur2612@gmail.com>
2023-10-13 13:00:46 +0600

    lecture practice of O(1)

---
+++ src/main/java/org/example/BigO_1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class BigO_1 {
+
+    public static int  printItems(int n) {
+       return n+n;
+
+    }
+
+    public static void main(String[] args) {
+        printItems(10);
+
+    }
+
+//T.C: O(1) no matter what the n value is it will only do 1 operation that is n+n
+    //O(1) also called constant operation as this is can be more than 1 operation
+}
