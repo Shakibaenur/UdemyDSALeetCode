7fe6cc478cc14265d7dc144f2d63ee67b00b326a
shakibaenur <shakibaenur2612@gmail.com>
2023-10-13 12:56:32 +0600

    lecture practice of drop non dominant rule

--- src/main/java/org/example/BigO_n2.java
+++ src/main/java/org/example/BigO_n2.java
@@ -8,11 +8,16 @@ public class BigO_n2 {
                 System.out.println(i+" "+j);
             }
         }
+
+            for (int k = 0; k < n; k++) {
+                System.out.println(k);
+            }
+
     }
 
     public static void main(String[] args) {
         printItems(10);
     }
 
-//T.C: O(n^2)
+//T.C: O(n^2) as n^2 times is the dominent this rule of simplification is called drop non-dominant
 }
