d512a46a2847c55fe5e4c565ffc2affc61eda1f0
shakibaenur <shakibaenur2612@gmail.com>
2023-10-24 11:44:11 +0600

    Leetcode Doubly LinedList constructor and append

---
+++ src/main/java/org/example/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,56 @@
+package org.example.DoublyLinkedList;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value){
+            this.value=value;
+        }
+    }
+    public DoublyLinkedList(int value){
+        Node newNode=new Node(value);
+        head=newNode;
+        tail=newNode;
+        length=1;
+    }
+
+    public void append(int value){
+        Node newNode=new Node(value);
+        if(length==0){
+            head=newNode;
+        }else{
+            tail.next=newNode;
+            newNode.prev=tail;
+        }
+        tail=newNode;
+        length++;
+    }
+    public void printList(){
+        Node temp= head;
+        while(temp!=null){
+            System.out.println(temp.value);
+            temp=temp.next;
+        }
+    }
+
+    public void getHead(){
+        System.out.println("Head: "+head);
+    }
+
+    public void getTail(){
+        System.out.println("Tail: "+ tail);
+    }
+
+    public void getLength(){
+        System.out.println("Length: "+length);
+    }
+
+}
---
+++ src/main/java/org/example/DoublyLinkedList/DoublyMain.java
@@ -0,0 +1,12 @@
+package org.example.DoublyLinkedList;
+
+public class DoublyMain {
+    public static void main(String[] args) {
+        DoublyLinkedList doublyLinkedList=new DoublyLinkedList(1);
+
+        doublyLinkedList.printList();
+
+        doublyLinkedList.append(10);
+        doublyLinkedList.printList();
+    }
+}
